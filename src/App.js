6c2f6f220b05d48110ddff899afeb671fa68e92f
hutingerica <hutingstudio@gmail.com>
2020-08-17 16:03:53 +0100

    toggle&delete

--- src/App.js
+++ src/App.js
@@ -12,14 +12,35 @@ function App(props) {
       id={task.id}
       name={task.name}
       completed={task.completed}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
       key={task.id}
       />
   ));
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   
   function addTask(name){
-    const newTask = {id: "todo-" + nanoid(), name: name, completed:"false" };
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false };
     console.log(newTask);
     setTasks([...tasks, newTask]);
+  };
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, "completed": !task.completed }
+      }
+      return task;
+    })
+    setTasks(updatedTasks);
+    console.log(updatedTasks)
+
+  };
+
+  function deleteTask(id){
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
   }
 
   return (
@@ -30,7 +51,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         // role="list"
         className="todo-list stack-large stack-exception"
@@ -52,3 +73,4 @@ export default App;
 //1.grab UIs to create todo.js > send props to todo.js > const Data in index.js > send Data to app.js > const taskList from Data and map into <Todo >
 //2.unique keys to each <Todo />
 //3.addTask function can grab props name into newTask and store into tasks array for taskList by using [tasks and setTasks]
+//4.countingList
